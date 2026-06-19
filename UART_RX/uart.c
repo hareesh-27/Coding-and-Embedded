b9bdd14bd4c90f0bdbe1d8c817c9d17392d1ebdc
Hareesh Aravind <hareesh27d@gmail.com>
2026-06-19 10:02:52 +0530

    Refactor UART initialization and remove duplicates
    Removed redundant macro definitions and combined UART RX and TX initialization into a single function.

--- UART_RX/uart.c
+++ UART_RX/uart.c
@@ -1,145 +1,77 @@
 #include "uart.h"
 
+#define GPIOAEN        (1U<<0)
+#define UART2EN        (1U<<17)
 
+#define CR1_RE         (1U<<2)
+#define CR1_UE         (1U<<13)
 
-#define GPIOAEN			(1U<<0)
-#define UART2EN			(1U<<17)
+#define SR_RXNE        (1U<<5)
 
-#define CR1_TE			(1U<<3)
-#define CR1_RE			(1U<<2)
+#define SYS_FREQ       16000000
+#define APB1_CLK       SYS_FREQ
 
-#define CR1_UE			(1U<<13)
-#define SR_TXE			(1U<<7)
-#define SR_RXNE			(1U<<5)
+#define UART_BAUDRATE  115200
 
+static void uart_set_baudrate(USART_TypeDef *USARTx,
+                              uint32_t PeriphClk,
+                              uint32_t BaudRate);
 
+static uint16_t compute_uart_bd(uint32_t PeriphClk,
+                                uint32_t BaudRate);
 
-#define SYS_FREQ		16000000
-#define APB1_CLK		SYS_FREQ
-
-#define UART_BAUDRATE		115200
-
-static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk,  uint32_t BaudRate);
-static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
-
-void uart2_write(int ch);
-
-
-int __io_putchar(int ch)
+/******************************************************
+ * UART2 RX Initialization
+ ******************************************************/
+void uart2_rx_init(void)
 {
-	uart2_write(ch);
-	return ch;
+    /* Enable clock access to GPIOA */
+    RCC->AHB1ENR |= GPIOAEN;
+
+    /* Set PA3 to Alternate Function Mode */
+    GPIOA->MODER &= ~(1U<<6);
+    GPIOA->MODER |=  (1U<<7);
+
+    /* Set PA3 Alternate Function to AF07 (USART2_RX) */
+    GPIOA->AFR[0] |= (1U<<12);
+    GPIOA->AFR[0] |= (1U<<13);
+    GPIOA->AFR[0] |= (1U<<14);
+    GPIOA->AFR[0] &= ~(1U<<15);
+
+    /* Enable clock access to USART2 */
+    RCC->APB1ENR |= UART2EN;
+
+    /* Configure baud rate */
+    uart_set_baudrate(USART2, APB1_CLK, UART_BAUDRATE);
+
+    /* Enable Receiver */
+    USART2->CR1 = CR1_RE;
+
+    /* Enable USART2 */
+    USART2->CR1 |= CR1_UE;
 }
 
-void uart2_rxtx_init(void)
-{
-	/****************Configure uart gpio pin***************/
-	/*Enable clock access to gpioa */
-	RCC->AHB1ENR |= GPIOAEN;
-
-	/*Set PA2 mode to alternate function mode*/
-	GPIOA->MODER &=~(1U<<4);
-	GPIOA->MODER |= (1U<<5);
-
-	/*Set PA2 alternate function type to UART_TX (AF07)*/
-	GPIOA->AFR[0] |= (1U<<8);
-	GPIOA->AFR[0] |= (1U<<9);
-	GPIOA->AFR[0] |= (1U<<10);
-	GPIOA->AFR[0] &= ~(1U<<11);
-
-
-	/*Set PA3 mode to alternate function mode*/
-	GPIOA->MODER &=~(1U<<6);
-	GPIOA->MODER |= (1U<<7);
-
-	/*Set PA3 alternate function type to UART_RX (AF07)*/
-	GPIOA->AFR[0] |= (1U<<12);
-	GPIOA->AFR[0] |= (1U<<13);
-	GPIOA->AFR[0] |= (1U<<14);
-	GPIOA->AFR[0] &= ~(1U<<15);
-
-
-
-
-	/****************Configure uart module ***************/
-	/*Enable clock access to uart2 */
-	RCC->APB1ENR |= UART2EN;
-
-	/*Configure baudrate*/
-	uart_set_baudrate(USART2,APB1_CLK,UART_BAUDRATE);
-
-	/*Configure the transfer direction*/
-	 USART2->CR1 =  (CR1_TE | CR1_RE);
-
-	/*Enable uart module*/
-	 USART2->CR1 |= CR1_UE;
-
-
-}
-
-void uart2_tx_init(void)
-{
-	/****************Configure uart gpio pin***************/
-	/*Enable clock access to gpioa */
-	RCC->AHB1ENR |= GPIOAEN;
-
-	/*Set PA2 mode to alternate function mode*/
-	GPIOA->MODER &=~(1U<<4);
-	GPIOA->MODER |= (1U<<5);
-
-	/*Set PA2 alternate function type to UART_TX (AF07)*/
-	GPIOA->AFR[0] |= (1U<<8);
-	GPIOA->AFR[0] |= (1U<<9);
-	GPIOA->AFR[0] |= (1U<<10);
-	GPIOA->AFR[0] &= ~(1U<<11);
-
-
-	/****************Configure uart module ***************/
-	/*Enable clock access to uart2 */
-	RCC->APB1ENR |= UART2EN;
-
-	/*Configure baudrate*/
-	uart_set_baudrate(USART2,APB1_CLK,UART_BAUDRATE);
-
-	/*Configure the transfer direction*/
-	 USART2->CR1 =  CR1_TE;
-
-	/*Enable uart module*/
-	 USART2->CR1 |= CR1_UE;
-
-
-}
-
-
+/******************************************************
+ * Read one character from UART2
+ ******************************************************/
 char uart2_read(void)
 {
-	  /*Make sure the receive data register is not empty*/
-	while(!(USART2->SR & SR_RXNE)){}
+    /* Wait until receive data register is not empty */
+    while(!(USART2->SR & SR_RXNE)){}
 
-	/*Read data*/
-	return USART2->DR;
-
+    /* Return received character */
+    return USART2->DR;
 }
 
-void uart2_write(int ch)
+static void uart_set_baudrate(USART_TypeDef *USARTx,
+                              uint32_t PeriphClk,
+                              uint32_t BaudRate)
 {
-  /*Make sure the transmit data register is empty*/
-	while(!(USART2->SR & SR_TXE)){}
-
-  /*Write to transmit data register*/
-	USART2->DR	=  (ch & 0xFF);
+    USARTx->BRR = compute_uart_bd(PeriphClk, BaudRate);
 }
 
-
-
-
-static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk,  uint32_t BaudRate)
+static uint16_t compute_uart_bd(uint32_t PeriphClk,
+                                uint32_t BaudRate)
 {
-	USARTx->BRR =  compute_uart_bd(PeriphClk,BaudRate);
+    return ((PeriphClk + (BaudRate/2U))/BaudRate);
 }
-
-static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate)
-{
-	return ((PeriphClk + (BaudRate/2U))/BaudRate);
-}
-
